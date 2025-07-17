5fff88c695b3a720f9eaa4864b43db56b4714522
barick <barick3018@gmail.com>
2025-07-17 11:21:01 +0530

    adding path

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 	return "welcome to the world of Python"
+@app.route('/adam')
+def adam():
+        return "welcome to the world of adam"
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5100, debug=true)
+        app.run(host='0.0.0.0', port=5100, debug=True)
